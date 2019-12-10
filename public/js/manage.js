fd199eb84453df1105e4e2c660d014812f942cf6
A <fregate@ngs.ru>
2019-12-10 08:51:34 +0700

    UI select participiants

--- public/js/manage.js
+++ public/js/manage.js
@@ -1,9 +1,27 @@
+var U = [];
 
 var fetchUsers = user => {
+    $("#select-users-btn").attr("disabled", "");
+
+    U = [];
+    $("#user-list").empty();
+
     var db = firebase.firestore();
     db.collection("users").get().then(snapshot => {
         snapshot.forEach(doc => {
+            U.push({name:doc.data().name, uid:doc.uid});
+            $("#user-list").append(`
+            <div class="list-group-item list-group-item-action">
+            ${doc.data().name}
+            </div>
+            `);
         });
+        $("#select-users-btn").removeAttr("disabled");
+
+        $(".list-group .list-group-item").on('click', function(e) {
+            $(e.target).toggleClass("active");
+         });
+    
     }).catch(error => {
         console.log(error);
     });
@@ -22,4 +40,11 @@ firebase.auth().onAuthStateChanged((user) => {
 });
 
 $(document).ready(() => {
+    $("#select-all-button").on('click', e => {
+        $(".list-group .list-group-item").addClass('active');
+    });
+    $("#clear-all-button").on('click', e => {
+        $(".list-group .list-group-item").removeClass('active');
+    });
+
 });
