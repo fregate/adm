3c913bd593035a17704346dc3bded046af7713de
A <fregate@ngs.ru>
2019-12-12 22:35:59 +0700

    Generate rule field

--- public/js/manage.js
+++ public/js/manage.js
@@ -4,7 +4,7 @@ var S = [];
 var ADM;
 
 var fetchUsers = user => {
-    $("#select-users-btn").attr("disabled", "");
+    $("#select-users-button").attr("disabled", "");
 
     U = [];
     $("#user-list").empty();
@@ -19,7 +19,7 @@ var fetchUsers = user => {
             </div>
             `);
         });
-        $("#select-users-btn").removeAttr("disabled");
+        $("#select-users-button").removeAttr("disabled");
 
         $(".list-group .list-group-item").on('click', function(e) {
             $(e.target).toggleClass("active");
@@ -40,6 +40,17 @@ var refreshAdm = user => {
     });
 };
 
+var getUserName = (usersCollection, uid) => {
+    var userName = "";
+    if (usersCollection.some(u => {
+        userName = u.name;
+        return u.uid == uid;
+    }))
+        return userName;
+    else
+        return "";
+};
+
 var buildField = () => {
     S = [];
     var modal = $('#modal-add-users');
@@ -51,7 +62,50 @@ var buildField = () => {
 
     modal.modal('hide');
 
+    if (S.length == 0)
+        return;
+
     // update document with new users
+    $("#rules-field > thead > tr").empty();
+    // build table header
+    $("#rules-field > thead > tr").append(`<th scope="col" class="text-center align-middle text-primary"><i class="fas fa-tree"></i></th>`);
+    S.forEach(uid => {
+        $("#rules-field > thead > tr").append(`
+        <th scope="col">${getUserName(U, uid)}</th>
+        `);
+    });
+    // build table body
+    $("#rules-field > tbody").empty();
+    S.forEach(uid => {
+        var getRow = () => {
+            var tds = `<th scope="row">${getUserName(U, uid)}</th>`;
+            S.forEach(u2 => {
+                if (u2 == uid) {
+                    tds += `<td class="text-center align-middle">
+                    <button type="button" class="gift btn text-light" data-toggle="button" disabled>
+                    <i class="fas fa-ban"></i>
+                    </button>
+                    </td>`;
+                } else {
+                    tds += `
+                    <td class="text-center align-middle">
+                    <button type="button" class="gift btn text-success" data-toggle="button" aria-pressed="false" autocomplete="off">
+                    <i class="fas fa-gift"></i>
+                    </button>
+                    </td>
+                    `;
+                }
+            });
+
+            return tds;
+        }
+        $("#rules-field > tbody").append(`<tr><${getRow()}/tr>`);
+    });
+
+    $("#rules-field .gift").on('click', e => {
+        $(e.currentTarget).toggleClass('text-success');
+        $(e.currentTarget).toggleClass('text-danger');
+    });
 };
 
 firebase.auth().onAuthStateChanged((user) => {
